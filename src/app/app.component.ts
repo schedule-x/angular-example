155cdcc7e17c50a779a2c093715164736fa628c6
Tom Österlund <tom.osterlund1@gmail.com>
2024-06-14 09:16:38 +0200

    add template example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalendarComponent} from "@schedule-x/angular";
+import {createCalendar, viewWeek} from "@schedule-x/calendar";
+import '@schedule-x/theme-default/dist/calendar.css'
+import {createEventModalPlugin} from "@schedule-x/event-modal";
+import {createDragAndDropPlugin} from "@schedule-x/drag-and-drop";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-example';
+  calendarApp = createCalendar({
+    events: [
+      {
+        id: '1',
+        title: 'Event 1',
+        start: '2024-06-11 03:00',
+        end: '2024-06-11 05:00',
+      },
+    ],
+    views: [viewWeek],
+    plugins: [createEventModalPlugin(), createDragAndDropPlugin()]
+  })
 }
